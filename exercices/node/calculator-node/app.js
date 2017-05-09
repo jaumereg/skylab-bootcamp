984ae35b0faa92618b3dd5f692b0561ad3883bc6
Jaume Regas <jaumeregas1@gmail.com>
2017-05-09 09:59:44 +0200

    gitignore added

--- exercices/node/calculator-node/app.js
+++ exercices/node/calculator-node/app.js
@@ -9,7 +9,7 @@ var a = require('./operands.js').operand1
 var b = require('./operands.js').operand2
 var results = require('./results.js')
 
-console.log('Today is: ' + moment().format('LLLL'));
+console.log('Today is: ' + moment().format('MMMM Do YYYY, h:mm:ss a'));
 results('sum', a, b, sum(a, b))
 results('subs', a, b, subs(a, b))
 results('mult', a, b, mult(a, b))
